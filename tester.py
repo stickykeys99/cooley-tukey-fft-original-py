1c6b3d504361fc0f7df2fa2b384e5660687b8807
stickykeys99 <35600782+stickykeys99@users.noreply.github.com>
2024-11-15 12:23:35 +0800

    feat: updates to tester

--- tester.py
+++ tester.py
@@ -4,18 +4,25 @@ from random import uniform
 
 LB = -10**5
 UB = 10**5
-A_SIZE = 360
+A_SIZE = 120_960
 
-A = [(uniform(LB,UB)+uniform(LB,UB)*1j) for _ in range(A_SIZE)]
+EPS = 5e-7 # tolerance value
 
-result = fft(A)
-result2 = fft_base(A)
+print(f'Testing implemention with tolerance {EPS}')
 
-EPS = 1e-7 # tolerance value
-for a,b in zip(result,result2):
-    d = abs(a-b)
-    if d >= EPS:
-        print('Tolerance value check failed')
-        exit()
+for _ in range(30):
+    A = [(uniform(LB,UB)+uniform(LB,UB)*1j) for _ in range(A_SIZE)]
 
-print(f'The two results are within {EPS}')
+    result = fft(A)
+    result2 = fft_base(A)
+
+    max_d = -float('inf')
+
+    for a,b in zip(result,result2):
+        d = abs(a-b)
+        if d >= EPS:
+            print(f'Failed with d: {d}')
+            exit()
+        max_d = max(d, max_d)
+
+    print(f'Passed, highest d computed: {d}')
